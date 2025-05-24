b8994deaebbc58eb19c40c0c0c7bf92305caa13d
ABDULLAHQAZI1930 <muhammadabdullahqazi313@gmail.com>
2025-05-24 15:42:03 +0500

    Create ui.cpp

---
+++ ui.cpp
@@ -0,0 +1,19 @@
+// ui.cpp
+#include <iostream>
+using namespace std;
+
+// Function to display the main menu
+void showMainMenu() {
+    cout << "\n====== Task Manager Menu ======\n";
+    cout << "1. Add Task\n";
+    cout << "2. View All Tasks\n";
+    cout << "3. Update Task\n";
+    cout << "4. Delete Task\n";
+    cout << "5. Mark Task as Completed\n";
+    cout << "6. Mark Task as Incomplete\n";
+    cout << "7. View Completed Tasks\n";
+    cout << "8. View Incomplete Tasks\n";
+    cout << "9. Search Task by ID\n";
+    cout << "0. Exit\n";
+    cout << "Enter your choice: ";
+}
